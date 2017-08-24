32204f559da047453ce019b13d6e842f58aeab7b
norubidium <mshen@cse.unsw.edu.au>
2017-08-24 18:15:51 +1000

    thu17

---
+++ tut05/thu17.py
@@ -0,0 +1,113 @@
+class ConferenceCenter(object):
+    """docstring for ConferenceCenter."""
+    def __init__(self):
+        super(ConferenceCenter, self).__init__()
+        self._courses = set()
+
+    def add_course(self, course):
+        self._courses.add(course)
+
+    def delete_course(self, course):
+        self._courses.remove(course)
+
+    def __str__(self):
+        courses = list(map(str, self._courses))
+        return '\n\n'.join(courses)
+
+
+class People(object):
+    """docstring for People."""
+    student_id = 0
+    def __init__(self, name, address, phone):
+        super(People, self).__init__()
+
+        self._name = name
+        self._address = address
+        self._phone = phone
+
+    def change_address(self, addr):
+        self._address = addr
+
+    def change_phone(self, phone):
+        self._phone = phone
+
+    def __str__(self):
+        return '%s #%d: %s' % (self.__class__.__name__, self._id, self._name)
+
+
+class Attendee(People):
+    """docstring for Attendee."""
+    student_id = 0
+    def __init__(self, name, address, phone):
+        super(Attendee, self).__init__(name, address, phone)
+        self._id = Attendee.student_id
+        Attendee.student_id += 1
+
+
+class Trainer(People):
+    """docstring for Trainer."""
+    staff_id = 0
+    def __init__(self, name, address, phone, field):
+        super(Trainer, self).__init__(name, address, phone)
+        self._id = Trainer.staff_id
+        self._field = field
+        Trainer.staff_id += 1
+
+    def change_field(self, field):
+        self._field = field
+# course = Course(....)
+# course.add(2) -> Course.add(course, 2)
+
+
+class Course(object):
+    """docstring for Course."""
+    def __init__(self, title, abstract, date, venue, trainer):
+        super(Course, self).__init__()
+        self._title = title
+        self._abstract = abstract
+        self._date = date
+        self._venue = venue
+        self._attendee = set()
+        self._trainer = trainer
+
+    def add_attendee(self, attendee):
+        self._attendee.add(attendee)
+
+    def drop_attendee(self, attendee):
+        self._attendee.remove(attendee)
+
+    def change_trainer(self, trainer):
+        self._trainer = trainer
+
+    def __str__(self):
+        s = list(map(str, self._attendee))
+        return 'NAME: %s\n Trainer: %s\n Attendees: %s' % (self._title, str(self._trainer), ', '.join(s))
+
+    def __eq__(self, other):
+        if isinstance(other, self.__class__):
+            if self._title == other._title:
+                return True
+        return False
+
+    def __hash__(self):
+        return self._title.__hash__()
+    # def add(self, num):
+    #     self.num += num
+
+tc = ConferenceCenter()
+aarthi = Trainer("aarthi", "a@cse", "000", "Software?")
+comp1531 = Course("COMP1531", "Software blah", "10/10/100000", "RC", aarthi)
+comp1521 = Course("COMP1521", "Software blah", "10/10/100000", "RC", aarthi)
+comp15311 = Course("COMP1531", "Software blah", "10/10/100000", "RC", aarthi)
+abdul = Attendee('Whatever', 'ab@cse', '5304781297384')
+abdul1 = Attendee('Whatever', 'ab@cse', '5304781297384')
+minjie = Attendee('Minjie', 'ms@cse', '3249813')
+comp1531.add_attendee(abdul)
+comp1531.add_attendee(abdul1)
+comp1531.add_attendee(minjie)
+
+tc.add_course(comp1521)
+tc.add_course(comp1531)
+tc.add_course(comp15311)
+
+print(tc)
