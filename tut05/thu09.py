1f0167ff0552be4c53fa977e2fb6ffac51513ba4
Minjie Shen <markshen5295@hotmail.com>
2017-08-29 22:20:40 +1000

    Update thu09.py

--- tut05/thu09.py
+++ tut05/thu09.py
@@ -2,40 +2,40 @@ class ConferenceCenter(object):
     """docstring for ConferenceCenter."""
     def __init__(self):
         super(ConferenceCenter, self).__init__()
-        self._courses = set()
+        self.__courses = set()
 
     def add_course(self, course):
-        self._courses.add(course)
+        self.__courses.add(course)
 
     def delete_course(self, course):
-        self._courses.remove(course)
+        self.__courses.remove(course)
 
     def __str__(self):
-        return '\n\n'.join(list(map(str, self._courses)))
+        return '\n\n'.join(list(map(str, self.__courses)))
 
 class Person(object):
     """docstring for Person."""
     def __init__(self, name, address, phone):
         super(Person, self).__init__()
-        self._name = name
-        self._address = address
-        self._phone = phone
+        self.__name = name
+        self.__address = address
+        self.__phone = phone
 
     def change_address(self, address):
-        self._address = address
+        self.__address = address
 
     def change_phone(self, phone):
-        self._phone = phone
+        self.__phone = phone
 
     def __str__(self):
-        return '%s #%d: %s' % (self.__class__.__name__, self._id, self._name)
+        return '%s #%d: %s' % (self.__class__.__name__, self.__id, self.__name)
 
 class Attendee(Person):
     """docstring for Attendee."""
     student_id = 0
     def __init__(self, name, address, phone):
         super(Attendee, self).__init__(name, address, phone)
-        self._id = Attendee.student_id
+        self.__id = Attendee.student_id
         Attendee.student_id += 1
 
 
@@ -44,47 +44,47 @@ class Trainer(Person):
     staff_id = 0
     def __init__(self, name, address, phone, field):
         super(Trainer, self).__init__(name, address, phone)
-        self._field = field
-        self._id = Trainer.staff_id
+        self.__field = field
+        self.__id = Trainer.staff_id
         Trainer.staff_id += 1
 
     def change_field(self, field):
-        self._field = field
+        self.__field = field
 
 
 class Course(object):
     """docstring for Course."""
     def __init__(self, title, abstract, date, venue, trainer):
         super(Course, self).__init__()
-        self._title = title
-        self._abstract = abstract
-        self._date = date
-        self._venue = venue
-        self._trainer = trainer
-        self._attendees = set()
+        self.__title = title
+        self.__abstract = abstract
+        self.__date = date
+        self.__venue = venue
+        self.__trainer = trainer
+        self.__attendees = set()
 
     def add_attendee(self, attendee):
-        self._attendees.add(attendee)
+        self.__attendees.add(attendee)
 
     def drop_attendee(self, attendee):
-        self._attendees.remove(attendee)
+        self.__attendees.remove(attendee)
 
     def change_trainer(self, trainer):
-        self._trainer = trainer
+        self.__trainer = trainer
 
     def __str__(self):
         students = list(map(str, self._attendees))
         # print('THIS IS THE STUDENTS LIST', ', '.join(students))
-        return 'COURSE NAME: %s, TRAINER: %s\n, STUDENTS: %s' % (self._title, str(self._trainer), ', '.join(students))
+        return 'COURSE NAME: %s, TRAINER: %s\n, STUDENTS: %s' % (self.__title, str(self.__trainer), ', '.join(students))
 
     def __eq__(self, other):
         if isinstance(other, self.__class__):
-            if self._title == other._title:
+            if self.__title == other.__title:
                 return True
         return False
 
     def __hash__(self):
-        return self._title.__hash__()
+        return self.__title.__hash__()
 
 system = ConferenceCenter()
 aarthi = Trainer("Aarthi", "a@cse.unsw.edu.au", "+61 000", "Theory")
